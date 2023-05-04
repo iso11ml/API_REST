fdd703a801eaaa2e8889fbf706ec7a6e243c995f
iso11ml <oungrad201@gmail.com>
2023-05-03 20:08:19 -0600

    add_ports

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+import uvicorn
 from routes.user import usersRouter
 from routes.article import articlesRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 app.include_router(usersRouter)
 app.include_router(articlesRouter) 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
